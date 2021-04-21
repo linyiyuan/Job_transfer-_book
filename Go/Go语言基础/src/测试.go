032e1634b75ea308be2a6a97b976094f98b74479
linyiyuan <linyiyuann@gmail.com>
2021-04-21 19:08:57 +0800

    测试代码

--- Go/Go语言基础/src/测试.go
+++ Go/Go语言基础/src/测试.go
@@ -1,18 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	num := 75
-	switch { // expression is omitted
-	case num >= 0 && num <= 50:
-		fmt.Println("num is greater than 0 and less than 50")
-	case num >= 51 && num <= 100:
-		fmt.Println("num is greater than 51 and less than 100")
-	case num >= 101:
-		fmt.Println("num is greater than 100")
+
+	str := "Hello"
+
+	for _, val := range str {
+		fmt.Println(val)
+		fmt.Println()
 	}
 
 }
