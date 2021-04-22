196280872b8df5a360679620bddecc62ea3eadd9
linyiyuan <linyiyuann@gmail.com>
2021-04-22 19:49:36 +0800

    测试代码

--- Go/Go语言基础/src/测试.go
+++ Go/Go语言基础/src/测试.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	var a [3]int
+	fmt.Println(a[0])
+	fmt.Println(a[len(a)-1])
 
-	str := "Hello"
-
-	for _, val := range str {
-		fmt.Println(val)
-		fmt.Println()
+	for k, v := range a {
+		fmt.Printf("键为：%v, 值为: %v", k, v)
 	}
 
 }
